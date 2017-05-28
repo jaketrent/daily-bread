99f28d403905ec4df492b9f6f217a954cf07ae33
jaketrent <trent.jake@gmail.com>
2017-05-28 17:11:16 -0600

    basic layout, styling, fonts

--- components/post.js
+++ components/post.js
@@ -1,14 +1,56 @@
-import Markdown from 'react-markdown'
 import 'isomorphic-fetch'
-
+import Markdown from 'react-markdown'
 import React from 'react'
 
 const Post = props =>
-  <div>
-    <h1>{props.title}</h1>
-    <time dateTime={props.date}>{props.date}</time>
-    <Markdown source={props.body} />
-    <a href={props.url}>{props.reference}</a>
+  <div className="root">
+    <div className="titleContainer">
+      <time className="date" dateTime={props.date}>{props.date}</time>
+      <h1 className="title">{props.title}</h1>
+    </div>
+    <div className="body">
+      <Markdown source={props.body} />
+    </div>
+    <a className="reference" href={props.url}>{props.reference}</a>
+    <style jsx>{`
+      .root {
+        display: inline-block;
+      }
+      .titleContainer {
+        display: flex;
+        align-items: center;
+        line-height: 42px;
+        color: #595a5b;
+      }
+      .date {
+        font-size: 12px;
+        margin-right: 18px;
+      }
+      .title {
+        margin: 0;
+        font-size: 24px;
+        font-family: "Milkshake";
+      }
+      .body {
+        font-size: 36px;
+        line-height: 42px;
+        font-family: "Questa_Regular";
+        color: #5ba9a9;
+        text-shadow: 1px 1px 0 #516a6f;
+      }
+      .body :global(p) {
+        margin: 0 0 24px 0;
+      }
+      .reference {
+        font-size: 24px;
+        color: #595a5b;
+        font-family: "Milkshake";
+        text-decoration: none;
+      }
+      .reference:hover {
+        text-decoration: underline;
+      }
+    `}</style>
   </div>
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 import Post from '../components/post'
 
-export default class extends React.Component {
+const FullBleedPage = props =>
+  <div className="full-bleed">
+        {props.children}
+    <style jsx>{`
+      .full-bleed {
+        height: calc(100vh - 48px);
+        width: calc(100vw - 48px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 24px;
+      }
+    `}</style>
+  </div>
+
+export default class Index extends React.Component {
   static async getInitialProps({ query }) {
     const post = await Post.fetchLatest()
     return { post }
@@ -8,8 +23,36 @@ export default class extends React.Component {
   render() {
     return (
       <div>
-        <div>Daily Bread 4</div>
-        <Post post={this.props.post} />
+        <FullBleedPage>
+          <Post post={this.props.post} />
+        </FullBleedPage>
+        <style global jsx>{`
+          @font-face {
+            font-family: "Milkshake";
+            src: url('/static/type/Milkshake.otf') format('opentype'),
+                 url('/static/type/Milkshake.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: "Questa_Regular";
+            src: url('/static/type/2-Questa_Regular-webfont.woff2') format('woff2'),
+                 url('/static/type/2-Questa_Regular-webfont.woff') format('woff');
+            font-weight: normal;
+            font-style: normal;
+          }
+          body {
+            box-sizing: border-box;
+            margin: 0;
+            font-family: sans-serif;
+            background: #EDF391;
+            background: linear-gradient(to bottom, #EDF391, #ead874);
+            color: #fff;
+          }
+          a {
+            color: #fff;
+          }
+        `}</style>
       </div>
     )
   }
